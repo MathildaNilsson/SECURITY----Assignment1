f188c256e0561022ca6dcf1d99317072bde71f54
Mathilda <frknilsson@hotmail.com>
2022-05-04 10:55:24 +0200

    added sql code

---
+++ README.md
@@ -0,0 +1,14 @@
+SQL injection
+Login
+
+Exploit:
+
+1.	Starta på startsidan
+2.	Användaren tar ett befintligt användarnamn ex: Brad men gör om det till Brad’—
+3.	Detta gör att …
+4.	Då har användaren lyckats logga in på användaren Brad utan att använda ett riktigt lösenord.
+
+Vulnerabilty: 
+
+
+Fix:
--- src/SqlInjection.java
+++ src/SqlInjection.java
@@ -1,2 +1,31 @@
 public class SqlInjection {
+    // Vulnerability
+
+/*    try (Connection c = db.getConnection()) {
+        Statement s = c.createStatement();
+        ResultSet rows = s.executeQuery(
+                "SELECT id FROM user WHERE username = '" + username + "' AND password = " + "'" + password + "'"
+        );
+
+        if (rows.next()) {
+            context.sessionAttribute("userId", rows.getInt("id"));
+            context.sessionAttribute("username", username);
+            context.redirect("/");
+        }*/
+
+    // Fix
+
+/*    String query = "SELECT id FROM user WHERE username =? AND password=?";
+    PreparedStatement s = c.prepareStatement(query);
+    s.setString(1,username);
+    s.setString(2,password);
+    ResultSet rows = s.executeQuery();
+
+     if (rows.next()) {
+        context.sessionAttribute("userId", rows.getInt("id"));
+        context.sessionAttribute("username", username);
+        context.redirect("/");*/
+
+
+
 }
